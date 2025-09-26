4347c4a715a07b67f8ca2f458b177e31fd6802e2
Petr Šácha <petrik.sacha@gmail.com>
2025-09-26 12:49:14 +0200

    keyboard functions

--- src/keyboard.rs
+++ src/keyboard.rs
@@ -1,10 +1,46 @@
+//! SDL_CategoryKeyboard:
+//!
+//! - [ ] SDL_ClearComposition
+//! - [ ] SDL_GetKeyboardFocus
+//! - [ ] SDL_GetKeyboardNameForID
+//! - [ ] SDL_GetKeyboards
+//! - [ ] SDL_GetKeyboardState
+//! - [ ] SDL_GetKeyFromName
+//! - [ ] SDL_GetKeyFromScancode
+//! - [x] SDL_GetKeyName
+//! - [x] SDL_GetModState
+//! - [ ] SDL_GetScancodeFromKey
+//! - [ ] SDL_GetScancodeFromName
+//! - [x] SDL_GetScancodeName
+//! - [ ] SDL_GetTextInputArea
+//! - [ ] SDL_HasKeyboard
+//! - [ ] SDL_HasScreenKeyboardSupport
+//! - [ ] SDL_ResetKeyboard
+//! - [ ] SDL_ScreenKeyboardShown
+//! - [ ] SDL_SetModState
+//! - [ ] SDL_SetScancodeName
+//! - [ ] SDL_SetTextInputArea
+//! - [x] SDL_StartTextInput
+//! - [ ] SDL_StartTextInputWithProperties
+//! - [x] SDL_StopTextInput
+//! - [x] SDL_TextInputActive
+
 use sdl3_sys::{
-    keyboard::{SDL_GetKeyName, SDL_GetScancodeName},
-    keycode::SDL_Keycode,
-    scancode::SDL_Scancode,
+    keyboard::{
+        SDL_GetKeyName, SDL_GetKeyboardState, SDL_GetModState, SDL_GetScancodeName,
+        SDL_StartTextInput, SDL_StopTextInput, SDL_TextInputActive,
+    },
+    keycode::{SDL_Keycode, SDL_Keymod},
+    scancode::{SDL_SCANCODE_COUNT, SDL_Scancode},
 };
 
-use crate::util::c_to_str;
+use crate::{
+    defs::SdlResult,
+    util::{c_to_str, to_result},
+    window::WindowRef,
+};
+
+const NUM_SCANCODES: usize = SDL_SCANCODE_COUNT.0 as usize;
 
 #[doc(alias = "SDL_GetScancodeName")]
 pub fn scancode_name(scancode: SDL_Scancode) -> &'static str {
@@ -14,4 +50,33 @@ pub fn scancode_name(scancode: SDL_Scancode) -> &'static str {
 #[doc(alias = "SDL_GetKeyName")]
 pub fn key_name(key: SDL_Keycode) -> &'static str {
     unsafe { c_to_str(SDL_GetKeyName(key)) }
+}
+
+#[doc(alias = "SDL_GetKeyboardState")]
+pub fn keyboard_state() -> &'static [bool; NUM_SCANCODES] {
+    unsafe {
+        (SDL_GetKeyboardState(std::ptr::null_mut()) as *const [bool; NUM_SCANCODES])
+            .as_ref()
+            .expect("SDL_GetKeyboardState returned a null pointer")
+    }
+}
+
+#[doc(alias = "SDL_GetModState")]
+pub fn mod_state() -> SDL_Keymod {
+    unsafe { SDL_GetModState() }
+}
+
+#[doc(alias = "SDL_StartTextInput")]
+pub fn text_input_start(wnd: impl Into<WindowRef>) -> SdlResult {
+    to_result(unsafe { SDL_StartTextInput(wnd.into().handle.as_ptr()) })
+}
+
+#[doc(alias = "SDL_StopTextInput")]
+pub fn text_input_stop(wnd: impl Into<WindowRef>) -> SdlResult {
+    to_result(unsafe { SDL_StopTextInput(wnd.into().handle.as_ptr()) })
+}
+
+#[doc(alias = "SDL_TextInputActive")]
+pub fn text_input_active(wnd: impl Into<WindowRef>) -> bool {
+    unsafe { SDL_TextInputActive(wnd.into().handle.as_ptr()) }
 }
