bcbdf8d76375f1e314afbba3c8194fc4a054e2d1
Petr Šácha <petrik.sacha@gmail.com>
2025-10-13 20:38:28 +0200

    error: get_{str, owned} utility functions

--- src/error.rs
+++ src/error.rs
@@ -1,6 +1,8 @@
 use std::{ffi::c_char, ptr::NonNull};
 
-use sdl3_sys::error::SDL_GetError;
+use sdl3_sys::error::{SDL_GetError, SDL_SetError};
+
+use crate::util::c_ptr_to_str;
 
 /// Returns a pointer to the contents of `SDL_GetError()`.
 ///
@@ -17,5 +19,33 @@ pub fn get() -> NonNull<c_char> {
     unsafe {
         NonNull::new(SDL_GetError().cast_mut())
             .expect("SDL_GetError() should never return a null pointer")
+    }
+}
+
+/// Returns `error::get()` as a `&'static str`, which isn't really
+/// sound, but hey, it's a convenience function, and the `unsafe`
+/// moniker should communicate that its usage is at your own risk.
+///
+/// Storing this value isn't recommended, as SDL's error buffer contents
+/// may change with any `SDL_SetError()` call at any point (although
+/// they're stored in a `static char[]` on the C side, so the address
+/// remains fixed).
+#[doc(alias = "SDL_GetError")]
+pub unsafe fn get_str() -> &'static str {
+    unsafe { c_ptr_to_str(get().as_ptr()) }
+}
+
+/// Returns `error::get()` as an owned `String`.
+#[doc(alias = "SDL_GetError")]
+pub fn get_owned() -> String {
+    unsafe { get_str() }.to_owned()
+}
+
+/// Not sure why you'd ever need this, but it's provided regardless
+/// for completeness' sake.
+#[doc(alias = "SDL_SetError")]
+pub fn set(err: &str) {
+    unsafe {
+        SDL_SetError(err.as_ptr().cast());
     }
 }
