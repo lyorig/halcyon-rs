fd3ed0ec26a6e1cd43860db045fd932f8a7102cb
Petr Šácha <petrsacha@proton.me>
2026-07-29 18:24:43 +0200

    impl Drop for Subsystem (with ManuallyDrop disclaimer)

--- src/subsystem.rs
+++ src/subsystem.rs
@@ -7,21 +7,36 @@ use crate::{Context, Result, error::Error};
 /// A handle to an SDL subsystem.
 /// This doesn't actually de-initialize the underlying subsystem upon being dropped;
 /// that's left up to the [`Context`] being destroyed. Thus, it cannot outlive it.
+///
+/// # Usage with [`std::mem::ManuallyDrop`]
+/// By default, dropping a subsystem de-initializes it.
+/// However, since dropping a [`Context`] de-initializes everything "by force"
+/// anyways, you can potentially avoid redundant FFI calls by wrapping this struct
+/// in [`std::mem::ManuallyDrop`].
 pub struct Subsystem<'ctx, const TYPE: u32> {
     marker: PhantomData<&'ctx Context>,
 }
 
 impl<const N: u32> Subsystem<'_, N> {
-    #[doc(alias = "SDL_Init")]
-    pub fn new(_: &Context) -> Result<Self> {
-        let res = unsafe { SDL_Init(SDL_InitFlags::new(N)) };
+    const FLAG: SDL_InitFlags = SDL_InitFlags::new(N);
+
+    #[doc(alias = "SDL_InitSubSystem")]
+    pub fn new<'ctx>(_: &'ctx Context) -> Result<Subsystem<'ctx, N>> {
+        let res = unsafe { SDL_InitSubSystem(Self::FLAG) };
         if res {
-            Ok(Self {
+            Ok(Subsystem {
                 marker: PhantomData,
             })
         } else {
             Err(Error::current())
         }
+    }
+}
+
+impl<const N: u32> Drop for Subsystem<'_, N> {
+    #[doc(alias = "SDL_QuitSubSystem")]
+    fn drop(&mut self) {
+        unsafe { SDL_QuitSubSystem(Self::FLAG) };
     }
 }
 
