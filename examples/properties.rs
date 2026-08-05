2dfdc94ba213432d35d0aa0b940b38ac4cd11584
Petr Šácha <petrsacha@proton.me>
2026-08-05 14:33:34 +0200

    examples: update properties

--- examples/properties.rs
+++ examples/properties.rs
@@ -1,11 +1,17 @@
+use std::f32::consts::PI;
+
 use halcyon::properties::Properties;
 
 fn run() -> halcyon::Result<()> {
     let props = Properties::new()?;
-    props.set_number(c"MyAge", 21)?;
-    props.set_string(c"ThisProjectName", c"halcyon-rs".as_ptr())?;
+    props.set_number(c"Integertastic", 20)?;
+    props.set_string(c"Placeholder", c"Lorem ipsum".as_ptr())?;
+    props.set_bool(c"Not a lie", true)?;
+    props.set_pointer(c"Null and void", std::ptr::null_mut())?;
+    props.set_float(c"All the digits", PI)?;
+
     props.enumerate(|k, v| {
-        halcyon::log!("Property {} = {}", k, v);
+        halcyon::log!("\"{k}\" = {v}");
     })?;
 
     Ok(())
