85bb0813398d7a82134e7229c8c172a187a2d52c
Petr Šácha <petrsacha@proton.me>
2026-08-06 14:34:44 +0200

    add doc aliases to builder methods

--- src/renderer/builder.rs
+++ src/renderer/builder.rs
@@ -16,6 +16,8 @@ impl RendererBuilder<'_> {
         RendererBuilder { inner }
     }
 
+    /// The name of the rendering driver to use, if a specific one is desired.
+    #[doc(alias = "SDL_PROP_RENDERER_CREATE_NAME_STRING")]
     pub fn name(&mut self, value: &CStr) -> &mut Self {
         let cstr = unsafe { CStr::from_ptr(SDL_PROP_RENDERER_CREATE_NAME_STRING) };
         _ = self.inner.set_string(cstr, value.as_ptr());
@@ -23,8 +25,10 @@ impl RendererBuilder<'_> {
         self
     }
 
-    /// The window where rendering is displayed. Mutually exclusive with
+    /// The window where rendering is displayed. Required if this isn't a
+    /// software renderer using a surface. Mutually exclusive with
     /// [`RendererBuilder::surface`].
+    #[doc(alias = "SDL_PROP_RENDERER_CREATE_WINDOW_POINTER")]
     pub fn window(&mut self, value: Ref<Window>) -> &mut Self {
         let cstr = unsafe { CStr::from_ptr(SDL_PROP_RENDERER_CREATE_WINDOW_POINTER) };
         _ = self.inner.set_pointer(cstr, value.handle.as_ptr().cast());
@@ -32,6 +36,9 @@ impl RendererBuilder<'_> {
         self
     }
 
+    /// The surface where rendering is displayed, if you want a software
+    /// renderer without a window.
+    #[doc(alias = "SDL_PROP_RENDERER_CREATE_SURFACE_POINTER")]
     pub fn surface(&mut self, value: Ref<Surface>) -> &mut Self {
         let cstr = unsafe { CStr::from_ptr(SDL_PROP_RENDERER_CREATE_SURFACE_POINTER) };
         _ = self.inner.set_pointer(cstr, value.handle.as_ptr().cast());
@@ -39,6 +46,9 @@ impl RendererBuilder<'_> {
         self
     }
 
+    /// An [`SDL_Colorspace`] value describing the colorspace for output to the
+    /// display. Defaults to `SDL_COLORSPACE_SRGB`.
+    #[doc(alias = "SDL_PROP_RENDERER_CREATE_OUTPUT_COLORSPACE_NUMBER")]
     pub fn colorspace(&mut self, value: SDL_Colorspace) -> &mut Self {
         let cstr = unsafe { CStr::from_ptr(SDL_PROP_RENDERER_CREATE_OUTPUT_COLORSPACE_NUMBER) };
         _ = self.inner.set_number(cstr, value.0.into());
@@ -46,6 +56,10 @@ impl RendererBuilder<'_> {
         self
     }
 
+    /// Non-zero if you want present synchronized with the refresh rate. This
+    /// property can take any value that is supported by `SDL_SetRenderVSync`
+    /// for the renderer.
+    #[doc(alias = "SDL_PROP_RENDERER_CREATE_PRESENT_VSYNC_NUMBER")]
     pub fn vsync(&mut self, value: i64) -> &mut Self {
         let cstr = unsafe { CStr::from_ptr(SDL_PROP_RENDERER_CREATE_PRESENT_VSYNC_NUMBER) };
         _ = self.inner.set_number(cstr, value);
--- src/window/builder.rs
+++ src/window/builder.rs
@@ -13,18 +13,28 @@ impl WindowBuilder<'_> {
         WindowBuilder { inner }
     }
 
+    /// True if the window should be always on top.
+    #[doc(alias = "SDL_PROP_WINDOW_CREATE_ALWAYS_ON_TOP_BOOLEAN")]
     pub fn always_on_top(&mut self, value: bool) -> &mut Self {
         self.set_bool(SDL_PROP_WINDOW_CREATE_ALWAYS_ON_TOP_BOOLEAN, value)
     }
 
+    /// True if the window has no window decoration.
+    #[doc(alias = "SDL_PROP_WINDOW_CREATE_BORDERLESS_BOOLEAN")]
     pub fn borderless(&mut self, value: bool) -> &mut Self {
         self.set_bool(SDL_PROP_WINDOW_CREATE_BORDERLESS_BOOLEAN, value)
     }
 
+    /// True if the "tooltip" and "menu" window types should be automatically
+    /// constrained to be entirely within display bounds (default), false if
+    /// no constraints on the position are desired.
+    #[doc(alias = "SDL_PROP_WINDOW_CREATE_CONSTRAIN_POPUP_BOOLEAN")]
     pub fn constrain_popup(&mut self, value: bool) -> &mut Self {
         self.set_bool(SDL_PROP_WINDOW_CREATE_CONSTRAIN_POPUP_BOOLEAN, value)
     }
 
+    /// True if the window will be used with an externally managed graphics context.
+    #[doc(alias = "SDL_PROP_WINDOW_CREATE_EXTERNAL_GRAPHICS_CONTEXT_BOOLEAN")]
     pub fn ext_gfx_context(&mut self, value: bool) -> &mut Self {
         self.set_bool(
             SDL_PROP_WINDOW_CREATE_EXTERNAL_GRAPHICS_CONTEXT_BOOLEAN,
@@ -32,94 +42,144 @@ impl WindowBuilder<'_> {
         )
     }
 
+    /// True if the window should accept keyboard input (defaults to true).
+    #[doc(alias = "SDL_PROP_WINDOW_CREATE_FOCUSABLE_BOOLEAN")]
     pub fn focusable(&mut self, value: bool) -> &mut Self {
         self.set_bool(SDL_PROP_WINDOW_CREATE_FOCUSABLE_BOOLEAN, value)
     }
 
+    /// True if the window should start in fullscreen mode at desktop resolution.
+    #[doc(alias = "SDL_PROP_WINDOW_CREATE_FULLSCREEN_BOOLEAN")]
     pub fn fullscreen(&mut self, value: bool) -> &mut Self {
         self.set_bool(SDL_PROP_WINDOW_CREATE_FULLSCREEN_BOOLEAN, value)
     }
 
+    /// The height of the window.
+    #[doc(alias = "SDL_PROP_WINDOW_CREATE_HEIGHT_NUMBER")]
     pub fn height(&mut self, value: i64) -> &mut Self {
         self.set_number(SDL_PROP_WINDOW_CREATE_HEIGHT_NUMBER, value)
     }
 
+    /// True if the window should start hidden.
+    #[doc(alias = "SDL_PROP_WINDOW_CREATE_HIDDEN_BOOLEAN")]
     pub fn hidden(&mut self, value: bool) -> &mut Self {
         self.set_bool(SDL_PROP_WINDOW_CREATE_HIDDEN_BOOLEAN, value)
     }
 
+    /// True if the window uses a high pixel density buffer if possible.
+    #[doc(alias = "SDL_PROP_WINDOW_CREATE_HIGH_PIXEL_DENSITY_BOOLEAN")]
     pub fn high_pixel_density(&mut self, value: bool) -> &mut Self {
         self.set_bool(SDL_PROP_WINDOW_CREATE_HIGH_PIXEL_DENSITY_BOOLEAN, value)
     }
 
+    /// True if the window should start maximized.
+    #[doc(alias = "SDL_PROP_WINDOW_CREATE_MAXIMIZED_BOOLEAN")]
     pub fn maximized(&mut self, value: bool) -> &mut Self {
         self.set_bool(SDL_PROP_WINDOW_CREATE_MAXIMIZED_BOOLEAN, value)
     }
 
+    /// True if the window is a popup menu.
+    #[doc(alias = "SDL_PROP_WINDOW_CREATE_MENU_BOOLEAN")]
     pub fn menu(&mut self, value: bool) -> &mut Self {
         self.set_bool(SDL_PROP_WINDOW_CREATE_MENU_BOOLEAN, value)
     }
 
+    /// True if the window will be used with Metal rendering.
+    #[doc(alias = "SDL_PROP_WINDOW_CREATE_METAL_BOOLEAN")]
     pub fn metal(&mut self, value: bool) -> &mut Self {
         self.set_bool(SDL_PROP_WINDOW_CREATE_METAL_BOOLEAN, value)
     }
 
+    /// True if the window should start minimized.
+    #[doc(alias = "SDL_PROP_WINDOW_CREATE_MINIMIZED_BOOLEAN")]
     pub fn minimized(&mut self, value: bool) -> &mut Self {
         self.set_bool(SDL_PROP_WINDOW_CREATE_MINIMIZED_BOOLEAN, value)
     }
 
+    /// True if the window is modal to its parent.
+    #[doc(alias = "SDL_PROP_WINDOW_CREATE_MODAL_BOOLEAN")]
     pub fn modal(&mut self, value: bool) -> &mut Self {
         self.set_bool(SDL_PROP_WINDOW_CREATE_MODAL_BOOLEAN, value)
     }
 
+    /// True if the window starts with grabbed mouse focus.
+    #[doc(alias = "SDL_PROP_WINDOW_CREATE_MOUSE_GRABBED_BOOLEAN")]
     pub fn mouse_grabbed(&mut self, value: bool) -> &mut Self {
         self.set_bool(SDL_PROP_WINDOW_CREATE_MOUSE_GRABBED_BOOLEAN, value)
     }
 
+    /// True if the window will be used with OpenGL rendering.
+    #[doc(alias = "SDL_PROP_WINDOW_CREATE_OPENGL_BOOLEAN")]
     pub fn opengl(&mut self, value: bool) -> &mut Self {
         self.set_bool(SDL_PROP_WINDOW_CREATE_OPENGL_BOOLEAN, value)
     }
 
+    /// A [`Window`] that will be the parent of this window. Required for
+    /// windows with the "tooltip", "menu", and "modal" properties.
+    #[doc(alias = "SDL_PROP_WINDOW_CREATE_PARENT_POINTER")]
     pub fn parent(&mut self, value: Ref<Window>) -> &mut Self {
         let cstr = unsafe { CStr::from_ptr(SDL_PROP_WINDOW_CREATE_PARENT_POINTER) };
         _ = self.inner.set_pointer(cstr, value.handle.as_ptr().cast());
         self
     }
 
+    /// True if the window should be resizable.
+    #[doc(alias = "SDL_PROP_WINDOW_CREATE_RESIZABLE_BOOLEAN")]
     pub fn resizable(&mut self, value: bool) -> &mut Self {
         self.set_bool(SDL_PROP_WINDOW_CREATE_RESIZABLE_BOOLEAN, value)
     }
 
+    /// The title of the window, in UTF-8 encoding.
+    #[doc(alias = "SDL_PROP_WINDOW_CREATE_TITLE_STRING")]
     pub fn title(&mut self, value: &CStr) -> &mut Self {
         let cstr = unsafe { CStr::from_ptr(SDL_PROP_WINDOW_CREATE_TITLE_STRING) };
         _ = self.inner.set_string(cstr, value.as_ptr());
         self
     }
 
+    /// True if the window is transparent in the areas with an alpha of 0.
+    #[doc(alias = "SDL_PROP_WINDOW_CREATE_TRANSPARENT_BOOLEAN")]
     pub fn transparent(&mut self, value: bool) -> &mut Self {
         self.set_bool(SDL_PROP_WINDOW_CREATE_TRANSPARENT_BOOLEAN, value)
     }
 
+    /// True if the window is a tooltip.
+    #[doc(alias = "SDL_PROP_WINDOW_CREATE_TOOLTIP_BOOLEAN")]
     pub fn tooltip(&mut self, value: bool) -> &mut Self {
         self.set_bool(SDL_PROP_WINDOW_CREATE_TOOLTIP_BOOLEAN, value)
     }
 
+    /// True if the window is a utility window, not showing in the task bar and
+    /// window list.
+    #[doc(alias = "SDL_PROP_WINDOW_CREATE_UTILITY_BOOLEAN")]
     pub fn utility(&mut self, value: bool) -> &mut Self {
         self.set_bool(SDL_PROP_WINDOW_CREATE_UTILITY_BOOLEAN, value)
     }
 
+    /// True if the window will be used with Vulkan rendering.
+    #[doc(alias = "SDL_PROP_WINDOW_CREATE_VULKAN_BOOLEAN")]
     pub fn vulkan(&mut self, value: bool) -> &mut Self {
         self.set_bool(SDL_PROP_WINDOW_CREATE_VULKAN_BOOLEAN, value)
     }
 
+    /// The width of the window.
+    #[doc(alias = "SDL_PROP_WINDOW_CREATE_WIDTH_NUMBER")]
     pub fn width(&mut self, value: i64) -> &mut Self {
         self.set_number(SDL_PROP_WINDOW_CREATE_WIDTH_NUMBER, value)
     }
 
+    /// The x position of the window, or `SDL_WINDOWPOS_CENTERED`. Defaults to
+    /// `SDL_WINDOWPOS_UNDEFINED`. Relative to the parent for windows with the
+    /// "tooltip" or "menu" property set.
+    #[doc(alias = "SDL_PROP_WINDOW_CREATE_X_NUMBER")]
     pub fn x(&mut self, value: i64) -> &mut Self {
         self.set_number(SDL_PROP_WINDOW_CREATE_X_NUMBER, value)
     }
 
+    /// The y position of the window, or `SDL_WINDOWPOS_CENTERED`. Defaults to
+    /// `SDL_WINDOWPOS_UNDEFINED`. Relative to the parent for windows with the
+    /// "tooltip" or "menu" property set.
+    #[doc(alias = "SDL_PROP_WINDOW_CREATE_Y_NUMBER")]
     pub fn y(&mut self, value: i64) -> &mut Self {
         self.set_number(SDL_PROP_WINDOW_CREATE_Y_NUMBER, value)
     }
