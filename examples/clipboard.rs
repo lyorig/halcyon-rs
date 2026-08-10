ab1ed5df0541017292ce4e4c28ad13bfb4babda4
Petr Šácha <petrsacha@proton.me>
2026-08-10 14:36:12 +0200

    examples: clipboard `run()` function

--- examples/clipboard.rs
+++ examples/clipboard.rs
@@ -1,16 +1,23 @@
-use halcyon::{Context, subsystem::Video};
+use halcyon::{Context, Result, subsystem::Video};
 
-fn main() {
+fn run() -> Result {
     let ctx = Context::new();
-    let _vid = Video::new(&ctx).expect("Cannot initialize video subsystem");
+    let _vid = Video::new(&ctx)?;
 
     halcyon::log!(
         "Clipboard content before the program was run: \"{}\"",
         halcyon::clipboard::text()
     );
 
-    halcyon::clipboard::set_text(c"And now I see, with eye serene")
-        .expect("Couldn't set clipboard text");
+    halcyon::clipboard::set_text(c"And now I see, with eye serene")?;
 
     halcyon::log!("New clipboard content: \"{}\"", halcyon::clipboard::text());
+
+    Ok(())
+}
+
+fn main() {
+    if let Err(e) = run() {
+        halcyon::log!("Something went wrong: {e}");
+    }
 }
