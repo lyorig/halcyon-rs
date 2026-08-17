30a21f55405d5728f56c586c81fc29b436524f2a
Petr Šácha <petrsacha@proton.me>
2026-08-17 13:07:32 +0200

    scripts: specify `-sdk macosx` for `xcrun`

--- scripts/macos/compile-metal-shaders.sh
+++ scripts/macos/compile-metal-shaders.sh
@@ -9,11 +9,11 @@ fi
 
 
 # examples/gpu.rs
-xcrun metal -o /tmp/triangle.ir -c examples/shaders/src/triangle.metal
-xcrun metal -o examples/shaders/triangle.metallib /tmp/triangle.ir
+xcrun -sdk macosx metal -o /tmp/triangle.ir -c examples/shaders/src/triangle.metal
+xcrun -sdk macosx metal -o examples/shaders/triangle.metallib /tmp/triangle.ir
 rm /tmp/triangle.ir
 
 # examples/teapot.rs
-xcrun metal -o /tmp/teapot.ir -c examples/shaders/src/teapot.metal
-xcrun metal -o examples/shaders/teapot.metallib /tmp/teapot.ir
+xcrun -sdk macosx metal -o /tmp/teapot.ir -c examples/shaders/src/teapot.metal
+xcrun -sdk macosx metal -o examples/shaders/teapot.metallib /tmp/teapot.ir
 rm /tmp/teapot.ir
