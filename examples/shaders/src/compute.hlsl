33926a787c2b9b3e24f21657aaaa1be78ae0a4ee
Petr Šácha <petrik.sacha@gmail.com>
2026-08-30 23:21:35 +0200

    example: fix compute on Windows

--- examples/shaders/src/compute.hlsl
+++ examples/shaders/src/compute.hlsl
@@ -1,10 +1,10 @@
 // Compile via `scripts/windows/compile-hlsl-shaders.ps1`.
 
-RWStructuredBuffer<uint> data : register(u0);
+RWByteAddressBuffer data : register(u0, space1);
 
 [numthreads(1, 1, 1)]
 void cs_main(uint3 tid : SV_DispatchThreadID) {
     if (tid.x == 0) {
-        data[0] += 1;
+        data.Store(0, data.Load(0) + 1);
     }
 }
