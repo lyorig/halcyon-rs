f7d684ba7bc632414cd963ba8e92e95d361cd8d3
Petr Šácha <petrsacha@proton.me>
2026-09-01 12:38:47 +0200

    rect: `map` + consts

--- src/rect.rs
+++ src/rect.rs
@@ -10,16 +10,30 @@ pub struct Point<T> {
     pub y: T,
 }
 
+impl<T: Copy> Point<T> {
+    pub const fn new(x: T, y: T) -> Self {
+        Self { x, y }
+    }
+
+    pub fn map<U: Copy, F: Fn(T) -> U>(self, f: F) -> Point<U> {
+        Point::new(f(self.x), f(self.y))
+    }
+}
+
 pub type PointI32 = Point<i32>;
 impl PointI32 {
     pub const ZERO: Self = Self::new(0, 0);
 
-    pub fn as_f32(self) -> PointF32 {
-        self.into()
+    pub const fn as_f32(self) -> PointF32 {
+        Point::new(self.x as f32, self.y as f32)
     }
 
-    pub fn as_sdl_ptr(&self) -> *const SDL_Point {
+    pub const fn as_sdl_ptr(&self) -> *const SDL_Point {
         std::ptr::from_ref(self).cast()
+    }
+
+    pub const fn from_sdl(rect: SDL_Point) -> Self {
+        unsafe { std::mem::transmute(rect) }
     }
 }
 
@@ -27,30 +41,28 @@ pub type PointF32 = Point<f32>;
 impl PointF32 {
     pub const ZERO: Self = Self::new(0., 0.);
 
-    pub fn as_i32(self) -> PointI32 {
-        self.into()
+    pub const fn as_i32(self) -> PointI32 {
+        Point::new(self.x as i32, self.y as i32)
     }
 
-    pub fn as_sdl_ptr(&self) -> *const SDL_FPoint {
+    pub const fn as_sdl_ptr(&self) -> *const SDL_FPoint {
         std::ptr::from_ref(self).cast()
+    }
+
+    pub const fn from_sdl(rect: SDL_FPoint) -> Self {
+        unsafe { std::mem::transmute(rect) }
     }
 }
 
 impl From<PointI32> for PointF32 {
     fn from(value: PointI32) -> Self {
-        Point::new(value.x as _, value.y as _)
+        value.as_f32()
     }
 }
 
 impl From<PointF32> for PointI32 {
     fn from(value: PointF32) -> Self {
-        Point::new(value.x as _, value.y as _)
-    }
-}
-
-impl<T: Copy> Point<T> {
-    pub const fn new(x: T, y: T) -> Self {
-        Self { x, y }
+        value.as_i32()
     }
 }
 
@@ -76,54 +88,22 @@ pub struct Rect<T> {
     pub size: Point<T>,
 }
 
-pub type RectI32 = Rect<i32>;
-impl RectI32 {
-    pub const ZEROED: Self = Self::xywh(0, 0, 0, 0);
-
-    pub fn as_f32(self) -> RectF32 {
-        self.into()
-    }
-
-    pub fn as_sdl_ptr(&self) -> *const SDL_Rect {
-        std::ptr::from_ref(self).cast()
-    }
-}
-
-pub type RectF32 = Rect<f32>;
-impl RectF32 {
-    pub const ZEROED: Self = Self::xywh(0., 0., 0., 0.);
-
-    pub fn as_i32(self) -> RectI32 {
-        self.into()
-    }
-
-    pub fn as_sdl_ptr(&self) -> *const SDL_FRect {
-        std::ptr::from_ref(self).cast()
-    }
-}
-
-impl From<RectI32> for RectF32 {
-    fn from(value: RectI32) -> Self {
-        Rect::new(value.pos.into(), value.size.into())
-    }
-}
-
-impl From<RectF32> for RectI32 {
-    fn from(value: RectF32) -> Self {
-        Rect::new(value.pos.into(), value.size.into())
-    }
-}
-
-impl<T: Default + Copy> Rect<T> {
+impl<T: Copy> Rect<T> {
     pub const fn new(pos: Point<T>, size: Point<T>) -> Self {
         Self { pos, size }
     }
 
-    /// Create a `Rect` with all fields specified.
+    /// Create a [`Rect`] with all fields specified.
     pub const fn xywh(x: T, y: T, w: T, h: T) -> Self {
         Self::new(Point::new(x, y), Point::new(w, h))
     }
 
+    pub fn map<U: Copy, F: Fn(Point<T>) -> Point<U>>(self, f: F) -> Rect<U> {
+        Rect::new(f(self.pos), f(self.size))
+    }
+}
+
+impl<T: Copy + Default> Rect<T> {
     /// Convenience function, identical to `Rect::xywh(x, y, T::default(), T::default())`.
     pub fn xy(x: T, y: T) -> Self {
         Self::xywh(x, y, T::default(), T::default())
@@ -132,6 +112,52 @@ impl<T: Default + Copy> Rect<T> {
     /// Convenience function, identical to `Rect::xywh(T::default(), T::default(), w, h)`.
     pub fn wh(w: T, h: T) -> Self {
         Self::xywh(T::default(), T::default(), w, h)
+    }
+}
+
+pub type RectI32 = Rect<i32>;
+impl RectI32 {
+    pub const ZEROED: Self = Self::xywh(0, 0, 0, 0);
+
+    pub const fn as_f32(self) -> RectF32 {
+        RectF32::new(self.pos.as_f32(), self.size.as_f32())
+    }
+
+    pub const fn as_sdl_ptr(&self) -> *const SDL_Rect {
+        std::ptr::from_ref(self).cast()
+    }
+
+    pub const fn from_sdl(rect: SDL_Rect) -> Self {
+        unsafe { std::mem::transmute(rect) }
+    }
+}
+
+pub type RectF32 = Rect<f32>;
+impl RectF32 {
+    pub const ZEROED: Self = Self::xywh(0., 0., 0., 0.);
+
+    pub const fn as_i32(self) -> RectI32 {
+        RectI32::new(self.pos.as_i32(), self.size.as_i32())
+    }
+
+    pub const fn as_sdl_ptr(&self) -> *const SDL_FRect {
+        std::ptr::from_ref(self).cast()
+    }
+
+    pub const fn from_sdl(rect: SDL_FRect) -> Self {
+        unsafe { std::mem::transmute(rect) }
+    }
+}
+
+impl From<RectI32> for RectF32 {
+    fn from(value: RectI32) -> Self {
+        value.as_f32()
+    }
+}
+
+impl From<RectF32> for RectI32 {
+    fn from(value: RectF32) -> Self {
+        value.as_i32()
     }
 }
 
